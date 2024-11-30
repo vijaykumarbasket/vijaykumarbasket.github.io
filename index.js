977e41becfec32e2ded05131feebc70a78d7cc1e
vijaykumarbasket <52071072+vijaykumarbasket@users.noreply.github.com>
2024-11-30 13:51:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';*/
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();
+/*const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();*/
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -22,14 +22,14 @@ scene.add( cube );
 camera.position.z = 5;
 
 
-controls.addEventListener( 'change', ()=>{renderer.render(scene, camera)} );
-controls.update();
+/*controls.addEventListener( 'change', ()=>{renderer.render(scene, camera)} );
+controls.update();*/
 
-/*function animate() {
+function animate() {
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 
-}*/
+}
