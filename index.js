691c7e35170e494670e0b29d32e225335c1951ae
vijaykumarbasket <52071072+vijaykumarbasket@users.noreply.github.com>
2024-11-30 14:11:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,17 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+const controls = new OrbitControls( camera, renderer.domElement );
+
+camera.position.set( 0, 20, 100 );
+controls.update();
 
 function animate() {
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	requestAnimationFrame( animate );
+
+	// required if controls.enableDamping or controls.autoRotate are set to true
+	controls.update();
 
 	renderer.render( scene, camera );
 
