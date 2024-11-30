0a68793d626f5e2118e1a8a558597769fec799ce
vijaykumarbasket <52071072+vijaykumarbasket@users.noreply.github.com>
2024-11-30 13:48:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+/*import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';*/
 
 
 const scene = new THREE.Scene();
