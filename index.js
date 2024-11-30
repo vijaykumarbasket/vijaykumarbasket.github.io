cbd9107abe1aee98dadc7677d05b8551eb4fe56c
vijaykumarbasket <52071072+vijaykumarbasket@users.noreply.github.com>
2024-11-30 13:42:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const controls = new OrbitControls( camera, renderer.domElement );
-
-const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
 
 const renderer = new THREE.WebGLRenderer();
