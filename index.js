5a56fef07bdc0656d9b7b8af25d47c461656b5c6
vijaykumarbasket <52071072+vijaykumarbasket@users.noreply.github.com>
2024-11-30 13:47:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.set( 0, 20, 100 );
+camera.position.z = 5;
 
 
 controls.addEventListener( 'change', ()=>{renderer.render(scene, camera)} );
