56b9e714e2317d777a8bbe6447fb0d89a623fc69
vijaykumarbasket <52071072+vijaykumarbasket@users.noreply.github.com>
2024-11-30 13:56:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 import * as THREE from 'three';
-/*import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';*/
-
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-/*const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();*/
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -20,10 +14,6 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
-
-
-/*controls.addEventListener( 'change', ()=>{renderer.render(scene, camera)} );
-controls.update();*/
 
 function animate() {
 
