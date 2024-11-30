f2edf2368fc44f235f89171c08c695bd815cf4f2
vijaykumarbasket <52071072+vijaykumarbasket@users.noreply.github.com>
2024-11-30 13:34:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import * as OrbitControls from 'three/addons/controls/OrbitControls.js';
-import * as GLTFLoader from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
