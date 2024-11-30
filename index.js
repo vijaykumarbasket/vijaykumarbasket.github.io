63aafaaf174fa3177c804678f2ef587963923e6d
vijaykumarbasket <52071072+vijaykumarbasket@users.noreply.github.com>
2024-11-30 14:05:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import * as GLTFLoader from 'three/addons/gltf';
+import { GLTFLoader } from 'three/addons/';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
