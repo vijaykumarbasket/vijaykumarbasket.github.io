939f2cd1f3a37c5b03d981b0519a09aaa4ef42e7
vijaykumarbasket <52071072+vijaykumarbasket@users.noreply.github.com>
2024-11-30 13:41:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@v0.149.0/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@v0.149.0/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const controls = new OrbitControls( camera, renderer.domElement );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -21,6 +22,9 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.set( 0, 20, 100 );
+
+
+controls.addEventListener( 'change', ()=>{renderer.render(scene, camera)} );
 controls.update();
 
 function animate() {
