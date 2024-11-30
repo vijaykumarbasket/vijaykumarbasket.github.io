998cea37c497706261fa6af3dddddc4af795a4ac
vijaykumarbasket <52071072+vijaykumarbasket@users.noreply.github.com>
2024-11-30 14:00:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-scene.add( cube );
 
 camera.position.z = 5;
 
